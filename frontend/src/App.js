e73856edf653512bae0fbcc7f8cd9c9a412eff8c
Beto Lopez <363alberto@gmail.com>
2024-08-14 21:32:26 -0400

    added navBar functionality, removed Arcade tab temporarily

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Canvas } from '@react-three/fiber';
 import Navbar from './components/Navbar';
 import Projects from './components/Projects';
 import About from './components/About';
-import Arcade from './components/Arcade';
+
 import Contact from './components/Contact';
 import FibonacciAnimation from './components/fibonacciAnimation';
 
@@ -14,15 +14,14 @@ function App() {
     <Router>
       <div className="App">
         <Navbar />
-        <Canvas>
-          <FibonacciAnimation />
-          <Switch>
-            <Route path="/projects" component={Projects} />
-            <Route path="/about" component={About} />
-            <Route path="/arcade" component={Arcade} />
-            <Route path="/contact" component={Contact} />
-          </Switch>
-        </Canvas>
+          <Canvas>
+            <Routes>
+                <Route path="/projects" component={Projects} />
+                <Route path="/about" component={About} />
+                <Route path="/contact" component={Contact} />
+            </Routes>
+            
+          </Canvas>
       </div>
     </Router>
   );
--- frontend/src/components/Arcade.js
+++
@@ -1,8 +0,0 @@
-// src/components/Projects.js (repeat for About.js, Arcade.js, and Contact.js)
-import React from 'react';
-
-const Arcade = () => {
-  return <div>Arcade Content</div>;
-};
-
-export default Arcade;
